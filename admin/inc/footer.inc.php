ad8bd6caae821cf7358c171e9b691448adcf1c8a
baroudeColombbus <69351692+baroudeColombbus@users.noreply.github.com>
2021-05-10 14:43:16 +0200

    suite

---
+++ admin/inc/footer.inc.php
@@ -0,0 +1,15 @@
+    <!-- footer -->
+    <footer class="text-muted py-5">
+        <div class="container">
+            <p class="float-end mb-1">
+            <a href="#">Retour vers le haut</a>
+            </p>
+            <p class="mb-1">Exam webforce 3 &copy; baroude</p>
+        </div>
+    </footer><!-- / footer -->
+
+    <!-- bootstrap js bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,13 @@
+
+<!-- header -->
+<?php require_once 'inc/header.inc.php'; ?>
+
+    <!-- main -->
+    <main class="container">
+
+
+    </main><!-- / main container -->
+
+
+<!-- footer -->
+<?php require_once 'inc/footer.inc.php'; ?>
